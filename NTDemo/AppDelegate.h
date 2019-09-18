2068cc52b3508158681c9ee4f5558b8979911d04
xiedacai <ntm_xyz@163.com>
2019-09-18 10:37:23 +0800

    Initial Commit

---
+++ NTDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NTDemo
+//
+//  Created by Ganjiuhui on 9/18/19.
+//  Copyright © 2019 Ganjiuhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
